c79cc2ac022d9bc865a3cfa397ba7ac0f7073a43
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-12-17 16:49:17 -0300

    Initial Commit

---
+++ AppleWatchRing/AppleWatchRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchRingApp.swift
+//  AppleWatchRing
+//
+//  Created by Bruno Oliveira on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

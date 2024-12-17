72796e2a31353225de7c7c9f380c302ebecc40ec
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-12-17 17:55:50 -0300

    initial Commit

--- AppleWatchRing/ContentView.swift
+++ AppleWatchRing/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress: CGFloat = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            ActivityRingView(progress: $progress)
+                .fixedSize()
         }
-        .padding()
     }
 }
 
